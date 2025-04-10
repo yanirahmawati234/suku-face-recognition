d3117ff1e5ca8303ad01254e1ca7783fe6149fbb
ilhamfs7 <muh.ilham.fs@gmail.com>
2025-04-10 11:58:30 +0700

    make mtcnn, python harus 3.10 kebawah minimaj 3.6

--- facedetectandcrop/main.py
+++ facedetectandcrop/main.py
@@ -3,15 +3,27 @@ import cv2
 import os
 import numpy as np
 from PIL import Image
+from mtcnn import MTCNN
 
-def crop_face(image):
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+# def crop_face(image):
+#     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+#     if len(faces) == 0:
+#         return None, "Tidak ada wajah terdeteksi."
+#     (x, y, w, h) = faces[0]
+#     face_image = image[y:y+h, x:x+w]
+#     return face_image, None
+
+def crop_face_with_mtcnn(image):
+    detector = MTCNN()
+    faces = detector.detect_faces(image)
 
     if len(faces) == 0:
         return None, "Tidak ada wajah terdeteksi."
-    (x, y, w, h) = faces[0]
+    
+    (x, y, w, h) = faces[0]['box']
     face_image = image[y:y+h, x:x+w]
     return face_image, None
 
@@ -28,18 +40,17 @@ if uploaded_file is not None and suku_name and person_name:
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_RGB2BGR)
     st.image(opencv_image, channels="BGR", caption="Gambar Asli", use_column_width=True)
 
-    face_image, error = crop_face(opencv_image)
+    # face_image, error = crop_face(opencv_image)
+    face_image, error = crop_face_with_mtcnn(opencv_image)
 
     if face_image is not None:
         st.image(face_image, channels="BGR", caption="Gambar Setelah Crop", use_column_width=True)
 
-        # Tombol simpan foto
         if st.button("Simpan Foto"):
             dataset_directory = 'dataset'
             original_directory = os.path.join(dataset_directory, 'original', suku_name, person_name)
             crop_directory = os.path.join(dataset_directory, 'crop', suku_name, person_name)
 
-            # Membuat direktori jika belum ada
             if not os.path.exists(dataset_directory):
                 os.makedirs(dataset_directory)
 
@@ -49,12 +60,10 @@ if uploaded_file is not None and suku_name and person_name:
             if not os.path.exists(crop_directory):
                 os.makedirs(crop_directory)
 
-            # Menyimpan gambar asli
             original_path = os.path.join(original_directory, uploaded_file.name)
             with open(original_path, "wb") as f:
                 f.write(uploaded_file.getbuffer())
 
-            # Menyimpan gambar hasil crop
             cropped_image_path = os.path.join(crop_directory, f"cropped_{uploaded_file.name}")
             cv2.imwrite(cropped_image_path, face_image)
 
